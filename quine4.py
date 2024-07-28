59e13df026c5ba4faee7410f5ddfb9162d4fd710
phovos <datadeve@outlook.com>
2024-07-27 22:36:49 -0500

    Update quine at runtime

--- quine4.py
+++ quine4.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 # Repository URL
-REPO_URL = 'https://github.com/MOONLAPSED/quine3.git'
+REPO_URL = 'https://github.com/MOONLAPSED/quine4.git'
 
 quine_template = 'import subprocess\nimport sys\nimport os\n\n# Repository URL\nREPO_URL = {repo_url}\n\nquine_template = {source_code}\n\ndef git_pull():\n    result = subprocess.run([\'git\', \'pull\'], capture_output=True, text=True)\n    return result.returncode == 0 and \'Already up to date\' not in result.stdout\n\ndef git_commit():\n    with open(__file__, \'w\') as f:\n        # Write the quine script, including this function and below lines\n        f.write(quine_template.format(repo_url=repr(REPO_URL), source_code=repr(quine_template)))\n    subprocess.run([\'git\', \'add\', __file__])\n    subprocess.run([\'git\', \'commit\', \'-m\', \'Update quine at runtime\'])\n    subprocess.run([\'git\', \'push\', \'--set-upstream\', \'origin\', \'master\'])\n\ndef initialize_repo():\n    if not os.path.exists(\'.git\'):\n        subprocess.run([\'git\', \'init\'])\n        subprocess.run([\'git\', \'remote\', \'add\', \'origin\', REPO_URL])\n        git_commit()\n\ndef quine():\n    if git_pull():\n        os.execv(sys.executable, [\'python\'] + sys.argv)\n    else:\n        print(quine_template.format(repo_url=repr(REPO_URL), source_code=repr(quine_template)))\n        git_commit()\n\nif __name__ == "__main__":\n    initialize_repo()\n    quine()\n'
 
